f4bf0418673ea6e40fb0f61a70e6ebd9214ebab9
nam anh tran van <namanh022@gmail.com>
2024-04-03 20:44:09 +0300

    first commit

---
+++ MediaPlayer.Core/Entity/Media/Audio.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaPlayer.Core.Entity
+{
+    public class Audio
+    {
+        
+    }
+}
